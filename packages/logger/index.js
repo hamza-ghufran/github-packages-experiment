1eb334c66bb77c65dd793fb934239b02bf64bcb9
Hamza Ghufran <hamzazebghufran@gmail.com>
2021-11-24 14:35:20 +0530

    test: [packages/logger] add comment

--- packages/logger/index.js
+++ packages/logger/index.js
@@ -2,6 +2,7 @@ const { transports, createLogger, format } = require('winston');
 
 const path = require('path');
 
+// test changes
 const options = {
   exitOnError: false,
   format: format.combine(
